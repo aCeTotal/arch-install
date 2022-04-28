1d0e340e7d2e281485a63e9810abc5e743c52102
aCeTotal <lars.oksendal@gmail.com>
2022-04-28 11:33:22 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -370,7 +370,7 @@ sed -i 's/#Color/Color\nILoveCandy/;s/^#ParallelDownloads.*$/ParallelDownloads =
 
 # Enabling various services.
 print "Enabling Reflector, automatic snapshots, BTRFS scrubbing and systemd-oomd."
-for service in reflector.timer snapper-timeline.timer snapper-cleanup.timer btrfs-scrub@-.timer btrfs-scrub@home.timer btrfs-scrub@var-log.timer btrfs-scrub@\\x2esnapshots.timer grub-btrfs.path systemd-oomd
+for service in sddm.service reflector.timer snapper-timeline.timer snapper-cleanup.timer btrfs-scrub@-.timer btrfs-scrub@home.timer btrfs-scrub@var-log.timer btrfs-scrub@\\x2esnapshots.timer grub-btrfs.path systemd-oomd
 do
     systemctl enable "$service" --root=/mnt &>/dev/null
 done
