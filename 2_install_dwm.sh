e115c6d4dd07bbdb00afffe67ac054c71a892a6e
aCeTotal <lars.oksendal@gmail.com>
2022-05-16 12:38:39 +0200

    Update 2_install_dwm.sh

--- 2_install_dwm.sh
+++ 2_install_dwm.sh
@@ -29,11 +29,14 @@ sudo systemctl enable --now auto-cpufreq.service
 sudo systemctl enable --now ModemManager.service
 
 # Install packages
-sudo pacman -S --noconfirm --needed xorg bibata-cursor-theme arc-gtk-theme papirus-icon-theme efibootmgr networkmanager-openconnect polkit-gnome dialog wpa_supplicant nm-connection-editor mtools dosfstools base-devel linux-zen-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync reflector acpi acpi_call edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font
+sudo pacman -S --noconfirm --needed xorg bibata-cursor-theme arc-gtk-theme papirus-icon-theme efibootmgr networkmanager-openconnect polkit-gnome dialog wpa_supplicant nm-connection-editor mtools dosfstools base-devel linux-zen-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils bash-completion openssh rsync reflector acpi acpi_call edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font
 # Install fonts
 sudo pacman -S --noconfirm --needed dina-font tamsyn-font bdf-unifont ttf-bitstream-vera ttf-croscore ttf-dejavu ttf-droid gnu-free-fonts ttf-ibm-plex ttf-liberation ttf-linux-libertine noto-fonts ttf-roboto tex-gyre-fonts ttf-ubuntu-font-family ttf-anonymous-pro ttf-cascadia-code ttf-fantasque-sans-mono ttf-fira-mono ttf-hack ttf-fira-code ttf-inconsolata ttf-jetbrains-mono ttf-monofur adobe-source-code-pro-fonts cantarell-fonts inter-font ttf-opensans gentium-plus-font ttf-junicode adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts noto-fonts-cjk noto-fonts-emoji ttf-font-awesome awesome-terminal-fonts
 #Intall programs
 sudo pacman -S --noconfirm --needed obs-studio picom nitrogen alacritty blender gimp libreoffice-still pacman-contrib pavucontrol lxappearance thunar volumeicon network-manager-applet blueman virt-manager qemu qemu-arch-extra archlinux-wallpaper
+
+#Install sound
+sudo pacman -S --noconfirm --needed pipewire-docs wireplumber pipewire pipewire-pulse pipewire-alsa
 
 #Install brave-browser
 yay -S brave-bin
@@ -151,7 +154,7 @@ sudo systemctl enable --now acpid
 usermod -aG libvirt lars
 sudo virsh net-autostart default
 
-#Adding dotfiles to .config
+#Adding dotfiles + dwmblocks to ~/.config
 cd ~/.config
 git clone https://github.com/aCeTotal/dotfiles.git
 git clone https://github.com/aCeTotal/dwmblocks.git
